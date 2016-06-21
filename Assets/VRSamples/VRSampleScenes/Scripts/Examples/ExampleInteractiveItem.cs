642b3fdb9b3914442c4473329bce02d5d149e872
adamjoyce <adamjoyce@outlook.com>
2016-06-21 12:53:11 +0100

    Added Unity's VR Samples and created a simple 'look at the cube' game

---
+++ Assets/VRSamples/VRSampleScenes/Scripts/Examples/ExampleInteractiveItem.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using VRStandardAssets.Utils;
+using System.Collections;
+
+namespace VRStandardAssets.Examples
+{
+    // This script is a simple example of how an interactive item can
+    // be used to change things on gameobjects by handling events.
+    public class ExampleInteractiveItem : MonoBehaviour
+    {
+        [SerializeField] private Material m_NormalMaterial;                
+        [SerializeField] private Material m_OverMaterial;                  
+        [SerializeField] private Material m_ClickedMaterial;               
+        [SerializeField] private Material m_DoubleClickedMaterial;         
+        [SerializeField] private VRInteractiveItem m_InteractiveItem;
+        [SerializeField] private Renderer m_Renderer;
+
+        private bool moving = false;
+
+
+        private void Awake ()
+        {
+            m_Renderer.material = m_NormalMaterial;
+        }
+
+
+        private void OnEnable()
+        {
+            m_InteractiveItem.OnOver += HandleOver;
+            m_InteractiveItem.OnOut += HandleOut;
+            m_InteractiveItem.OnClick += HandleClick;
+            m_InteractiveItem.OnDoubleClick += HandleDoubleClick;
+        }
+
+
+        private void OnDisable()
+        {
+            m_InteractiveItem.OnOver -= HandleOver;
+            m_InteractiveItem.OnOut -= HandleOut;
+            m_InteractiveItem.OnClick -= HandleClick;
+            m_InteractiveItem.OnDoubleClick -= HandleDoubleClick;
+        }
+
+
+        //Handle the Over event
+        private void HandleOver()
+        {
+            Debug.Log("Show over state");
+            if (!moving) {
+                m_Renderer.material = m_OverMaterial;
+                moving = true;
+                StartCoroutine(WaitAndMove());
+            }
+        }
+
+
+        //Handle the Out event
+        private void HandleOut()
+        {
+            Debug.Log("Show out state");
+            m_Renderer.material = m_NormalMaterial;
+        }
+
+
+        //Handle the Click event
+        private void HandleClick()
+        {
+            Debug.Log("Show click state");
+            m_Renderer.material = m_ClickedMaterial;
+        }
+
+
+        //Handle the DoubleClick event
+        private void HandleDoubleClick()
+        {
+            Debug.Log("Show double click");
+            m_Renderer.material = m_DoubleClickedMaterial;
+        }
+
+        //
+        private IEnumerator WaitAndMove() {
+            yield return new WaitForSeconds(1);
+            MoveObject();
+        }
+
+        //
+        private void MoveObject() {
+            Transform objectTransform = m_InteractiveItem.transform;
+            Vector3 newPosition = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-20.0f, 0.0f));
+            objectTransform.position = newPosition;
+            moving = false;
+        }
+    }
+}
