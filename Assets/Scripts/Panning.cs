d4b9caccd1132a5990583dbc737cc4096c85fb0b
adamjoyce <adamjoyce@outlook.com>
2016-06-21 18:29:23 +0100

    Added panning (right cube) - needs tweaking and refactoring into single script

--- Assets/Scripts/Panning.cs
+++ Assets/Scripts/Panning.cs
@@ -11,13 +11,15 @@ public class Panning : MonoBehaviour {
 
     public Vector previousLeftHandPosition;
 
-    private float pitchIncrement = 1.0f;
+    private float panningIncrement = 1.0f;
+    private GameObject musicState;
 
     // Use this for initialization
     void Start() {
         controller = GetComponent<LeapServiceProvider>().GetLeapController();
         hands = new List<Hand>();
         audio = GameObject.Find("AudioSource");
+        musicState = GameObject.Find("MusicState");
     }
 
     // Update is called once per frame
@@ -26,19 +28,22 @@ public class Panning : MonoBehaviour {
         Frame frame = controller.Frame();
         if (frame.Hands.Count > 0) {
             hands = frame.Hands;
-            AdjustPitch();
+
+            if (musicState.GetComponent<MusicState>().currentMusicProptery != null && musicState.GetComponent<MusicState>().currentMusicProptery.name == "Panning") {
+                AdjustPanning();
+            }
             previousLeftHandPosition = hands[0].PalmPosition;
         }
     }
 
     //
-    private void AdjustPitch() {
+    private void AdjustPanning() {
         if (previousLeftHandPosition == null) {
             // Do nothing.
-        } else if (previousLeftHandPosition.z < hands[0].PalmPosition.z - pitchIncrement) {
-            audio.GetComponent<AudioSource>().pitch -= 0.005f;
-        } else if (previousLeftHandPosition.z > hands[0].PalmPosition.z + pitchIncrement) {
-            audio.GetComponent<AudioSource>().pitch += 0.005f;
+        } else if (previousLeftHandPosition.z < hands[0].PalmPosition.x - panningIncrement) {
+            audio.GetComponent<AudioSource>().panStereo -= 0.05f;
+        } else if (previousLeftHandPosition.z > hands[0].PalmPosition.x + panningIncrement) {
+            audio.GetComponent<AudioSource>().panStereo += 0.05f;
         }
     }
 }
