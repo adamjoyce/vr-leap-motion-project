b086f5e1fff56f1594febf24751126d65f8f4d94
Ciaran <mr.ciaranwilson@gmail.com>
2016-06-22 15:28:31 +0100

    first cloth sim tests
    promising results so far, all changes in Ciarans test scene

---
+++ Assets/Scripts/TextureScroll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureScroll : MonoBehaviour {
+    float scrollSpeed = 0.5f;
+    float offset;
+    float rotate;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset += (Time.deltaTime * scrollSpeed) / 10.0f;
+        gameObject.GetComponent<Renderer>().material.SetTextureOffset("_MainTex",new Vector2(offset, 0));
+	
+	}
+}
