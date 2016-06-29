0366b44ca16c750f07349345113dc84630e1c9d7
BaggioPereira <baggio_111111@hotmail.com>
2016-06-29 11:54:13 +0100

    Uses pitch to change texture scroll speed

--- Assets/Scripts/TextureScroll.cs
+++ Assets/Scripts/TextureScroll.cs
@@ -2,18 +2,25 @@
 using System.Collections;
 
 public class TextureScroll : MonoBehaviour {
-    float scrollSpeed = 1.0f;
+    public float scrollSpeed = 1.0f;
     float offset;
     float rotate;
+    AudioSource aSource;
 	// Use this for initialization
 	void Start () {
-	
+        aSource = FindObjectOfType<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        updateScrollSpeed();
         offset += (Time.deltaTime * scrollSpeed) / 10.0f;
         gameObject.GetComponent<Renderer>().material.SetTextureOffset("_MainTex",new Vector2(offset, 0));
 	
 	}
+
+    void updateScrollSpeed()
+    {
+        scrollSpeed = aSource.pitch;
+    }
 }
