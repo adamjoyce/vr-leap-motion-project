4ca4e80c445a936f4423c061c002f684ec3db563
BaggioPereira <baggio_111111@hotmail.com>
2016-07-03 23:02:25 +0100

    Functions to change state

--- Assets/Scripts/Recorder.cs
+++ Assets/Scripts/Recorder.cs
@@ -27,5 +27,43 @@ namespace Leap.Unity
         protected List<byte[]> frames;
         protected float frameIndex;
         protected Frame currentFrame = new Frame();
+
+        //New recorder, reset variables
+        public void Recorder()
+        {
+            Reset();
+        }
+
+        //Set the state to Stopped
+        public void Stop()
+        {
+            state = RecorderState.Stopped;
+            frameIndex = 0.0f;
+        }
+
+        //Set the state to Paused
+        public void Pause()
+        {
+            state = RecorderState.Paused;
+        }
+
+        //Set the state to Playing
+        public void Play()
+        {
+            state = RecorderState.Playing;
+        }
+
+        //Set the state to Recording
+        public void Record()
+        {
+            state = RecorderState.Recording;
+        }
+
+        //Reset variables
+        public void Reset()
+        {
+            frames = new List<byte[]>();
+            frameIndex = 0.0f;
+        }
     }
 }
