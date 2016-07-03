cec7b69d4e48bbc145daa6f64c85de1a047a96b0
BaggioPereira <baggio_111111@hotmail.com>
2016-07-03 22:55:14 +0100

    Recorder script initial setup

---
+++ Assets/Scripts/Recorder.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Leap;
+
+namespace Leap.Unity
+{
+    //States
+    public enum RecorderState
+    {
+        Recording = 0,
+        Playing = 1,
+        Paused = 2,
+        Stopped = 3,
+    }
+
+    //Recorder class
+    public class Recorder
+    {
+        public float playbackSpeed = 1.0f;
+        public bool isLooping = true;
+
+        public RecorderState state = RecorderState.Playing;
+
+        protected List<byte[]> frames;
+        protected float frameIndex;
+        protected Frame currentFrame = new Frame();
+    }
+}
