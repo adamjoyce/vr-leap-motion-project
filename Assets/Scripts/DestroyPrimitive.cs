08f974809757bd9f4e5a96436af20ee756e1b313
Ciaran <mr.ciaranwilson@gmail.com>
2016-07-04 18:37:14 +0100

    video texture added

--- Assets/Scripts/DestroyPrimitive.cs
+++ Assets/Scripts/DestroyPrimitive.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 
 public class DestroyPrimitive : MonoBehaviour
 {
@@ -19,25 +19,9 @@ public class DestroyPrimitive : MonoBehaviour
     {
         if (Vector3.Distance(transform.position, player.transform.position) > destroyDistance)
         {
-            List<ClothSphereColliderPair> clothColliders = new List<ClothSphereColliderPair>(cloth.GetComponent<Cloth>().sphereColliders);
-
-            // Remove the cloth sphere collider pair related to this sphere's collider.
-            int index = 0;
-            bool notRemoved = true;
-            while (notRemoved)
-            {
-                if (clothColliders[index].first == gameObject.GetComponent<SphereCollider>())
-                {
-                    clothColliders.RemoveAt(index);
-                    notRemoved = false;
-                }
-                index++;
-            }
-
-            cloth.GetComponent<Cloth>().sphereColliders = clothColliders.ToArray();
-            Debug.Log("Removed Index: " + index);
-
             Destroy(gameObject);
+
+            // Remove the sphere collider from the cloth component.
         }
         //Debug.Log(Vector3.Distance(transform.position, player.transform.position));
     }
---
+++ Assets/Scripts/Video.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Video : MonoBehaviour
+{
+    public MovieTexture movTexture;
+    void Start()
+    {
+        GetComponent<Renderer>().material.mainTexture = movTexture;
+        movTexture.Play();
+    }
+}
