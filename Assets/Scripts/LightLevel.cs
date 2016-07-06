b3ac60c8d1a612b3cb0932742c93285fa9237261
BaggioPereira <baggio_111111@hotmail.com>
2016-07-06 10:09:03 +0100

    Light level script working

--- Assets/Scripts/LightLevel.cs
+++ Assets/Scripts/LightLevel.cs
@@ -3,31 +3,26 @@ using System.Collections;
 
 public class LightLevel : MonoBehaviour
 {
-    Light light;
+    float light;
     AudioSource aSource;
 	// Use this for initialization
 	void Start ()
     {
         aSource = FindObjectOfType<AudioSource>();
-        light = gameObject.GetComponent<Light>();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        changeLightLevel();
-	}
-
-    void changeLightLevel()
-    {
-        if(light.intensity > 0.0f && light.intensity < 1.0f)
+        light = aSource.pitch;
+        light = (light - (-1.5f)) / (1.5f - (-1.5f));
+        if (light > 0.0f && light < 1.0f)
         {
-            if (light.intensity <= 0.0f)
-                light.intensity = 0.1f;
-            else if (light.intensity >= 1.0f)
-                light.intensity = 0.99f;
-            else
-                light.intensity = aSource.pitch;
+            if (light <= 0.0f)
+                light = 0.1f;
+            else if (light >= 1.0f)
+                light = 0.99f;
+            gameObject.GetComponent<Light>().intensity = light;
         }
-    }
+	}
 }
