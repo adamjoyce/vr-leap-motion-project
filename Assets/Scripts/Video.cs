e8f7a1a4903a23813f8c84d2b506eeb263f6b9d0
BaggioPereira <baggio_111111@hotmail.com>
2016-07-06 13:54:33 +0100

    Video.cs has second MovieTexture

--- Assets/Scripts/Video.cs
+++ Assets/Scripts/Video.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class Video : MonoBehaviour
 {
     public MovieTexture movTexture;
+    public MovieTexture movTexture2;
     void Start()
     {
         GetComponent<Renderer>().material.mainTexture = movTexture;
         movTexture.Play();
+        GetComponent<Renderer>().material.SetTexture("_SecondaryTex", movTexture2);
+        movTexture2.Play();
     }
 }
