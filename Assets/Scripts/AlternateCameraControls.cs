a7117dea37b7e6cda7d5a80679f8d9ecf552f4ba
Adam Joyce <adamjoyce@outlook.com>
2016-07-22 11:08:34 +0100

    Added script for VR independent camera rotation

---
+++ Assets/Scripts/AlternateCameraControls.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlternateCameraControls : MonoBehaviour
+{
+    public float horizontalSpeed = 2.0f;
+    public float verticalSpeed = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            yaw += horizontalSpeed * Input.GetAxis("Mouse X");
+            pitch += verticalSpeed * Input.GetAxis("Mouse Y");
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+    }
+}
