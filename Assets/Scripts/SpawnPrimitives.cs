6168dc939cfffd39a22c197724712e8bd10d2bda
adamjoyce <adamjoyce@outlook.com>
2016-07-25 12:42:15 +0100

    Bubble generation and scalling working well

--- Assets/Scripts/SpawnPrimitives.cs
+++ Assets/Scripts/SpawnPrimitives.cs
@@ -18,7 +18,14 @@ public class SpawnPrimitives : MonoBehaviour
     public float wristDistance = 2.0f;
     public float spawnDelay = 1.0f;
     public float kinematicDelay = 0.5f;
+
+    public float minSphereSize = 0.1f;
+    public float maxSphereSize = 0.35f;
+
     public bool delay = false;
+    public bool sphereAttached = false;
+
+    private GameObject sphere;
 
     // Use this for initialization.
     void Start()
@@ -40,11 +47,41 @@ public class SpawnPrimitives : MonoBehaviour
         hands = provider.CurrentFrame.Hands;
         if (hands.Count > 1)
         {
-            if (hands[0].WristPosition.DistanceTo(hands[1].WristPosition) < wristDistance && !delay)
+            if (hands[0].WristPosition.DistanceTo(hands[1].WristPosition) < wristDistance && !delay && !sphereAttached)
             {
                 StartCoroutine(SpawnSphere());
+                sphereAttached = true;
             }
-            //Debug.Log(hands[0].WristPosition.DistanceTo(hands[1].WristPosition));
+            else if (sphereAttached)
+            {
+                sphere.transform.position = (hands[0].PalmPosition.ToVector3() + hands[1].PalmPosition.ToVector3()) * 0.5f;
+                float scaleDistance = hands[0].WristPosition.DistanceTo(hands[1].WristPosition) * 0.6f;
+
+                if (sphere.transform.localScale.x <= minSphereSize)
+                {
+                    Vector3 newScale = new Vector3(scaleDistance, scaleDistance, scaleDistance);
+                    if (newScale.x >= sphere.transform.localScale.x)
+                        sphere.transform.localScale = new Vector3(scaleDistance, scaleDistance, scaleDistance);
+                }
+                else if (sphere.transform.localScale.x >= maxSphereSize)
+                {
+                    Vector3 newScale = new Vector3(scaleDistance, scaleDistance, scaleDistance);
+                    if (newScale.x <= sphere.transform.localScale.x)
+                        sphere.transform.localScale = new Vector3(scaleDistance, scaleDistance, scaleDistance);
+                }
+                else
+                {
+                    sphere.transform.localScale = new Vector3(scaleDistance, scaleDistance, scaleDistance);
+                }
+
+                if (!hands[1].Fingers[0].IsExtended && !hands[1].Fingers[1].IsExtended && !hands[1].Fingers[2].IsExtended && !hands[1].Fingers[3].IsExtended && !hands[1].Fingers[4].IsExtended &&
+                    !hands[0].Fingers[0].IsExtended && !hands[0].Fingers[1].IsExtended && !hands[0].Fingers[2].IsExtended && !hands[0].Fingers[3].IsExtended && !hands[0].Fingers[4].IsExtended)
+                {
+                    sphereAttached = false;
+                    if (sphere.transform.localScale.x < minSphereSize) 
+                        sphere.transform.localScale = new Vector3(minSphereSize, minSphereSize, minSphereSize);
+                }
+            }
         }
     }
 
@@ -57,7 +94,7 @@ public class SpawnPrimitives : MonoBehaviour
         Vector3 spawnPos = (firstPalmPos + secondPalmPos) * 0.5f;
 
         GameObject.Find("AudioPop").GetComponent<AudioSource>().Play();
-        GameObject sphere = Instantiate(bubbleSphere, spawnPos, Quaternion.identity) as GameObject;
+        sphere = Instantiate(bubbleSphere, spawnPos, Quaternion.identity) as GameObject;
 
         // Setup the bubble spheres rigidbody properties.
         sphere.AddComponent<Rigidbody>();
