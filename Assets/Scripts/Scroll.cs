3b98dbafb74a7234a170e36d45443cfe5c47fc9a
BaggioPereira <baggio_111111@hotmail.com>
2016-07-26 21:37:28 +0100

    Revert "Removed old scroll scripts"
    This reverts commit f8ee320644f8306fbb22ec079549eb93f961df2e.

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+    float scrollamount;
+    public float speedOffset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        scrollamount += (Time.deltaTime * 0.5f) / speedOffset;
+        if (scrollamount >= 1.0f)
+            scrollamount = 0.0f;
+        gameObject.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(0, -scrollamount));
+        gameObject.GetComponent<Renderer>().material.SetTextureOffset("_DispTex", new Vector2(0, -scrollamount));
+        gameObject.GetComponent<Renderer>().material.SetTextureOffset("_NormalMap", new Vector2(0, -scrollamount));
+        gameObject.GetComponent<Renderer>().material.SetTextureOffset("_SecondaryTex", new Vector2(0, -scrollamount));
+        gameObject.GetComponent<Renderer>().material.SetTextureOffset("_SecondaryDisp", new Vector2(0, -scrollamount));
+        gameObject.GetComponent<Renderer>().material.SetTextureOffset("_SecondaryNormal", new Vector2(0, -scrollamount));
+	
+	}
+}
