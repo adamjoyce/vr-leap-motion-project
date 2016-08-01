7ce316c43fc51a794ea19098b903f1ba8e9f8d81
BaggioPereira <baggio_111111@hotmail.com>
2016-08-01 11:45:01 +0100

    Added gizmo viewer to bubbles

---
+++ Assets/GizmoViewer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GizmoViewer : MonoBehaviour {
+    PopBubble popBub;
+    float radius;
+	// Use this for initialization
+	void Start () 
+    {
+        popBub = FindObjectOfType<PopBubble>();
+        radius = popBub.explosionRadius;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        radius = popBub.explosionRadius;
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(gameObject.transform.position, radius);
+    }
+}
