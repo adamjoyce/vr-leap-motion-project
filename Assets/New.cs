292c83f68d0c97e7b5764d40dcdd335b6b756f29
BaggioPereira <baggio_111111@hotmail.com>
2016-08-17 13:42:39 +0100

    BubbleEmission and emission video

---
+++ Assets/New.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class New : MonoBehaviour {
+    public MovieTexture mainTex;
+	// Use this for initialization
+	void Start () 
+    {
+        gameObject.GetComponent<Renderer>().material.SetTexture("_Emission", mainTex);
+        mainTex.Play();
+        mainTex.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/BubbleEmission.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleEmission : MonoBehaviour {
+    Renderer bubbleRend;
+    Material bubbleMat;
+    Color emissionColor;
+    public float emissionStrength;
+	// Use this for initialization
+	void Start () 
+    {
+        bubbleRend = gameObject.GetComponent<Renderer>();
+        bubbleMat = bubbleRend.material;
+        emissionColor = new Color(0.14f,0.89f,1.0f);
+
+        emissionStrength = 17.6f;
+        bubbleMat.SetColor("_EmissionColor", emissionColor * emissionStrength);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        bubbleMat.SetColor("_EmissionColor", emissionColor * emissionStrength);
+        // emission strength set to value of B channel
+	}
+}
