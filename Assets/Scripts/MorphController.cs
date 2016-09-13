66b5aeb14a3b9139533746d3308adef72c4a11c0
adamjoyce <adamjoyce@outlook.com>
2016-09-13 17:21:57 +0100

    Basic lerping morph targets for agents working

--- Assets/Scripts/MorphController.cs
+++ Assets/Scripts/MorphController.cs
@@ -3,16 +3,31 @@ using System.Collections;
 
 public class MorphController : MonoBehaviour
 {
+    SkinnedMeshRenderer morphMesh;
+    float minMorphValue = 1.0f;
+    float maxMorphValue = 100.0f;
+    float time = 0.0f;
 
     // Use this for initialization
     void Start()
     {
-
+        morphMesh = GetComponent<SkinnedMeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        morphMesh.SetBlendShapeWeight(0, Mathf.Lerp(minMorphValue, maxMorphValue, time));
+        morphMesh.SetBlendShapeWeight(1, Mathf.Lerp(minMorphValue, maxMorphValue, time));
+        morphMesh.SetBlendShapeWeight(2, Mathf.Lerp(minMorphValue, maxMorphValue, time));
+        time += 0.5f * Time.deltaTime;
 
+        if (time > 1.0f)
+        {
+            float temp = minMorphValue;
+            minMorphValue = maxMorphValue;
+            maxMorphValue = temp;
+            time = 0.0f;
+        }
     }
 }
