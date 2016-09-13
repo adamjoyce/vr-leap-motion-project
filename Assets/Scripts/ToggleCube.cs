47215b8ad718b52aa3531c0f968d325c4ec58cf4
adamjoyce <adamjoyce@outlook.com>
2016-09-13 16:12:44 +0100

    Added test scene for new channel / tone system and added morph model for flocking agents

---
+++ Assets/Scripts/ToggleCube.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleCube : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100) && hit.transform.gameObject.tag == "ToneCube")
+            {
+                // Toggle audio on cube.
+                AudioSource audio = hit.transform.gameObject.GetComponent<AudioSource>();
+                if (audio.enabled)
+                    audio.enabled = false;
+                else
+                    audio.enabled = true;
+            }
+        }
+    }
+}
