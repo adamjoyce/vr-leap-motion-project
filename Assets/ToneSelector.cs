af2ceace92a16e10c2028a089b7e2254b13e9bd0
BaggioPereira <baggio_111111@hotmail.com>
2016-09-17 19:35:45 +0100

    Added playscene and new bubble prefab

---
+++ Assets/ToneSelector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToneSelector : MonoBehaviour {
+    public AudioClip[] clips;
+	// Use this for initialization
+	void Start () 
+    {
+        GetComponent<AudioSource>().clip = clips[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
