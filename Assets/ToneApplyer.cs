03dafc77b82a8a7a4070fa0d06f23baddbe2a2aa
BaggioPereira <baggio_111111@hotmail.com>
2016-09-19 11:25:34 +0100

    Using colour predefined

--- Assets/ToneApplyer.cs
+++ Assets/ToneApplyer.cs
@@ -5,6 +5,7 @@ public class ToneApplyer : MonoBehaviour {
     ToneSelector selector;
     float combined;
     float[] bubbleSpectrum = new float[128];
+    Color color;
 	// Use this for initialization
 	void Start () 
     {
@@ -12,6 +13,17 @@ public class ToneApplyer : MonoBehaviour {
         GetComponent<AudioSource>().clip = selector.selected;
         GetComponent<AudioSource>().Play();
         GetComponent<AudioSource>().loop = true;
+        color = GetComponent<Renderer>().material.GetColor("_EmissionColor");
+        float highestValue;
+        if (color.r > color.g)
+            highestValue = color.r;
+        else
+            highestValue = color.g;
+        if (color.g > color.b)
+            highestValue = color.g;
+        else
+            highestValue = color.b;
+        color = new Color(color.r / highestValue, color.g / highestValue, color.b / highestValue);
 	}
 	
 	// Update is called once per frame
@@ -23,7 +35,7 @@ public class ToneApplyer : MonoBehaviour {
             combined += bubbleSpectrum[i];
         }
         combined *= 100;
-        GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.white*combined);
+        GetComponent<Renderer>().material.SetColor("_EmissionColor", color*combined);
         combined = 0;
 	}
 }
